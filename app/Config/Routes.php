c82c0bcc3bf0ced083bf7dd4cf1d9487e27a2c42
Eduardo <eduardo.sanchez31@outlook.com>
2025-08-26 22:05:31 -0400

    Estructura principal, docker-compose, dockerfile y configuracion basica

---
+++ app/Config/Routes.php
@@ -0,0 +1,16 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+
+// $routes->group('api', function ($routes) {
+    $routes->get('/tasks', 'TaskController::index');
+    $routes->get('/tasks/(:num)', 'TaskController::show/$1');
+    $routes->post('/tasks', 'TaskController::store');
+    $routes->put('/tasks/(:num)', 'TaskController::update/$1');
+    $routes->delete('/tasks/(:num)', 'TaskController::delete/$1');
+// });
